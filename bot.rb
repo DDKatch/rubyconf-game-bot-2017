94ac8c3419a57d66bc08b008afe0e6a6452b8b00
DDKatch <daniil.field@gmail.com>
2017-04-01 17:10:22 +0300

    refactored version

--- bot.rb
+++ bot.rb
@@ -1,11 +1,10 @@
 require 'github_api'
 require 'pry'
-require "google/cloud/vision"
+require 'google/cloud/vision'
 require 'watir'
 
 @browser = Watir::Browser.new
 @browser.goto 'https://play.rubyconference.by/game'
-# @browser.goto 'file:///home/chakra/work/kek.html'
 
 # Your Google Cloud Platform project ID
 PROJECT_ID = "i-enterprise-163118"
@@ -37,9 +36,8 @@ def github_search_query(search_value)
   %Q{language:ruby is:pr is:merged in:title "#{search_value}"} 
 end
 
-def img2text(img_path)
-  # system %Q{xclip -selection clipboard -t image/png -o > #{img_path}}
-  text = @vision.image(img_path).text.text.split(/\n/).join(' ')
+def img2text(img_url)
+  text = @vision.image(img_url).text.text.split(/\n/).join(' ')
 end
 
 def compared_str(source_s, github_s)
@@ -56,8 +54,7 @@ end
 
 def click_answer(text)
   @browser.button(text: text).click
-  @browser.wait(30)
-  click_next_task
+  game_over? ? restart_game! : click_next_task
 end
 
 def try(object, method)
@@ -80,44 +77,53 @@ def pr_title_img_old?
   try(pr_title_img, :src) != @old_img_url
 end
 
-# @head_img_counter = 14
+def game_over?
+  @browser.div(class: 'game-finish-button').exist?
+end
+
+def restart_game!
+  @browser.goto 'https://play.rubyconference.by/game'
+  @head_img_counter = 0
+  @browser.wait_until(200){ any_header_exist? }
+  @header = ''
+  @old_img_url = '' 
+  game_loop
+end
+
+def game_loop
+  until !game_over? do
+    if @head_img_counter < 15 
+      @browser.wait_until(60) { pr_title_old? }
+    else
+      @browser.wait_until(60) { pr_title_img_old? }
+      @old_img_url = pr_title_img.src 
+    end
+
+    @header = pr_title_img.exist? ? img2text(try(pr_title_img, :src)) : pr_title.text 
+    @head_img_counter += 1
+
+    searcher = Github::Client::Search.new(oauth_token: '2362f396ced84ded220b4ea19cbdc8a9fab13383')
+    result = -1
+    result = searcher.issues(q: github_search_query(@header)).body.items.inject(0) do |res_count, issue|
+      p words_from(issue[:title])
+      res_count += compared_str(@header, issue[:title]) ? 1 : 0
+    end
+
+    result += 1 if EXCEPTIONS_MERGED.include?(@header)
+    result = 0 if EXCEPTIONS_REJECTED.include?(@header)
+    puts "result: #{result}"
+    p words_from(@header)
+    puts github_search_query(@header)
+    result > 0 ? p('MERGED') : p('REJECTED')
+    puts "COUNTER: #{@head_img_counter}\nOLD_IMG_URL: #{@old_img_url}"
+    result > 0 ? click_answer('Merged') : click_answer('Rejected')
+  end
+  restart_game!
+end
+
 @head_img_counter = 0
 @browser.wait_until(200){ any_header_exist? }
 @header = ''
 @old_img_url = '' 
 
-loop do
-  if 
-  if @head_img_counter < 15 
-    @browser.wait_until(6) { pr_title_old? }
-  else
-    @browser.wait_until(10) { pr_title_img_old? }
-    @old_img_url = pr_title_img.src 
-  end
-
-  @header = pr_title_img.exist? ? img2text(try(pr_title_img, :src)) : pr_title.text 
-
-  @head_img_counter += 1
-  # system "sleep 2"
-
-  # @@header = img_level ? img2text(`xclip -o`) : `xclip -o` 
-
-  searcher = Github::Client::Search.new(oauth_token: 'af7661daa1ae7eb870e8a0089812a2d533bc0b83')
-  result = -1
-  result = searcher.issues(q: github_search_query(@header)).body.items.inject(0) do |res_count, issue|
-    p words_from(issue[:title])
-    res_count += compared_str(@header, issue[:title]) ? 1 : 0
-  end
-
-  # @browser.wait_until{ result != -1 }
-
-  # @browser.wait(20)
-  result += 1 if EXCEPTIONS_MERGED.include?(@header)
-  result = 0 if EXCEPTIONS_REJECTED.include?(@header)
-  puts "result: #{result}"
-  p words_from(@header)
-  puts github_search_query(@header)
-  result > 0 ? p('MERGED') : p('REJECTED')
-  puts "COUNTER: #{@head_img_counter}\nOLD_IMG_URL: #{@old_img_url}"
-  result > 0 ? click_answer('Merged') : click_answer('Rejected')
-end
+game_loop
